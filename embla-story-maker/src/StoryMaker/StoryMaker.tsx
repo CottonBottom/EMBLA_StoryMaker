96ef1ee39d7a19086b7ce3730ec7c1d5ac87878a
CottonBottom <87255233+CottonBottom@users.noreply.github.com>
2021-10-30 07:26:44 +0900

    Updated data
    Updated data

--- embla-story-maker/src/StoryMaker/StoryMaker.tsx
+++ embla-story-maker/src/StoryMaker/StoryMaker.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
-import data from "./Data.json";
+import storyData from "./StoryData.json";
+import namingData from "./NamingData.json";
+import creatures from "./GauntletData/Creatures.json";
+import deity from "./GauntletData/Creatures.json";
+import planes from "./GauntletData/Creatures.json";
+import tribes from "./GauntletData/Creatures.json";
+
+const gauntletData = creatures.gauntlets.concat(
+  deity.gauntlets,
+  planes.gauntlets,
+  tribes.gauntlets
+);
 
 const StoryMaker = () => {
   const [rarity, setRarity] = useState<string>("no rarity");
@@ -8,7 +19,7 @@ const StoryMaker = () => {
 
   const getRarities = () => {
     const options: JSX.Element[] = [<option hidden>select</option>];
-    data.rarity.forEach((rarity) => {
+    storyData.rarity.forEach((rarity) => {
       options.push(<option value={rarity.content}>{rarity.name}</option>);
     });
     return options;
@@ -16,7 +27,7 @@ const StoryMaker = () => {
 
   const getElements = () => {
     const options: JSX.Element[] = [<option hidden>select</option>];
-    data.element.forEach((element) => {
+    storyData.element.forEach((element) => {
       options.push(<option value={element.content}>{element.name}</option>);
     });
     return options;
@@ -24,7 +35,7 @@ const StoryMaker = () => {
 
   const getAttacks = () => {
     const options: JSX.Element[] = [<option hidden>select</option>];
-    data.attack.forEach((attack) => {
+    storyData.attack.forEach((attack) => {
       options.push(<option value={attack.content}>{attack.name}</option>);
     });
     return options;
@@ -32,6 +43,78 @@ const StoryMaker = () => {
 
   const makeStory = () => {
     return `${rarity} ${attack} ${element}`;
+  };
+
+  const capitalizeFirstLetter = (name: string) => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  };
+
+  const makeName = (rarity: string, element: string, attack: string) => {
+    const start = namingData.rarity.filter(
+      (_rarity) => _rarity.name === rarity
+    )[0].name;
+    const middle = namingData.element.filter(
+      (_element) => _element.name === element
+    )[0].content;
+    const end = namingData.attack.filter(
+      (_attack) => _attack.name === attack
+    )[0].content;
+
+    return `${capitalizeFirstLetter(start)}'s ${middle} ${end}`;
+  };
+
+  const getFirstLetter = (word: string) => {
+    return word.charAt(0).toUpperCase() + word.charAt(1);
+  };
+
+  const makeID = (rarity: string, element: string, attack: string) => {
+    const start = namingData.rarity.filter(
+      (_rarity) => _rarity.name === rarity
+    )[0].name;
+    const middle = namingData.element.filter(
+      (_element) => _element.name === element
+    )[0].content;
+    const end = namingData.attack.filter(
+      (_attack) => _attack.name === attack
+    )[0].content;
+
+    return `${getFirstLetter(start)}_${getFirstLetter(middle)}_${getFirstLetter(
+      end
+    )}`;
+  };
+
+  const makeStoryCell = (rarity: string, element: string, attack: string) => {
+    const start = storyData.rarity.filter(
+      (_rarity) => _rarity.name === rarity
+    )[0].content;
+    const middle = storyData.attack.filter(
+      (_attack) => _attack.name === attack
+    )[0].content;
+    const end = storyData.element.filter(
+      (_element) => _element.name === element
+    )[0].content;
+
+    return `${start} ${middle} ${end}`;
+  };
+
+  const makeRows = () => {
+    return gauntletData.map((gauntlet, index) => {
+      return (
+        <tr>
+          <td>{index + 1}</td>
+          <td>
+            {makeName(gauntlet.rarity, gauntlet.element, gauntlet.attack)}
+          </td>
+          <td>{gauntlet.rarity}</td>
+          <td>{gauntlet.element}</td>
+          <td>{gauntlet.attack}</td>
+          <td>
+            {makeStoryCell(gauntlet.rarity, gauntlet.element, gauntlet.attack)}
+          </td>
+          <td>{makeID(gauntlet.rarity, gauntlet.element, gauntlet.attack)}</td>
+        </tr>
+      );
+    });
   };
 
   return (
@@ -72,6 +155,21 @@ const StoryMaker = () => {
         <label htmlFor="result">Result</label>
         <textarea value={makeStory()} rows={5} />
       </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Rarity</th>
+            <th>Element</th>
+            <th>Attack</th>
+            <th>Story</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>{makeRows()}</tbody>
+      </table>
     </div>
   );
 };
