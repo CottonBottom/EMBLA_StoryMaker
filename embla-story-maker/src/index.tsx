40cfd8760b2290f15ac51c7ff2b8c4c6f302a174
CottonBottom <87255233+CottonBottom@users.noreply.github.com>
2021-11-03 07:55:47 +0900

    doll maker
    doll maker

--- embla-story-maker/src/index.tsx
+++ embla-story-maker/src/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import StoryMaker from "./StoryMaker/StoryMaker";
+import DollMaker from "./DollMaker/DollMaker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoryMaker />
+    <Router>
+      <div className="menu">
+        <Link to="/story">Story Maker</Link>
+        <Link to="/dolls">Broken Dolls Maker</Link>
+      </div>
+      <Route exact path="/story">
+        <StoryMaker />
+      </Route>
+      <Route exact path="/dolls">
+        <DollMaker />
+      </Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
